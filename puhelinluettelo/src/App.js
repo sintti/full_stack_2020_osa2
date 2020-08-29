863ab2b8658993f60fae676f50c9e40889c04ea4
sintti <erkka.karjalainen@gmail.com>
2020-08-29 13:05:26 +0300

    Haetaan puhelinluettelon data db.json tiedostosta käyttäen axionia

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-        { name: 'Sintti Pinttinen', number: '0401201201', id: 1 },
-        { name: 'Arto Hellas', number: '040-123456', id: 2 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 3 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 4 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 5 }
-    ])
+    const [ persons, setPersons ] = useState([])
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
     const [ newFilter, setNewFilter ] = useState('')
-    const [ showAll, setShowAll ] = useState('')
+    const [ showAll, setShowAll ] = useState()
+    
+    // Get data from db.json with axios
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
     
     // Changelistener for name
     const handleNameChange = (event) => {
