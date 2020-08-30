03b932cc67b420e995411a7c5a55b202fb5b48bc
sintti <erkka.karjalainen@gmail.com>
2020-08-30 11:08:43 +0300

    Maiden tiedot 2.13: show/hide nappi, jos haku näyttää enemmän kuin yhden maan

--- maiden_tiedot/src/components/Countries.js
+++ maiden_tiedot/src/components/Countries.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Country from './Country'
+
+const ManyCountries = (props) =>  {
+    const [ showCountry, setShowCountry ] = useState(false)
+    
+    const countryToShow = showCountry
+        ? props.country
+        : []
+
+    return (
+        <div>
+            {props.country.name}
+            <button onClick={() => setShowCountry(!showCountry)}>
+            {showCountry ? 'Hide' : 'Show'}
+            </button>
+            {showCountry
+            ? <div>
+            <Country key={countryToShow.alpha3Code} country={countryToShow} />
+            </div>
+            : ''
+            }
+        </div>
+        
+    )
+}
 
 const Countries = (props) => {
     if (props.countries.length >= 10) {
         return (
             <p>Too many countries. Specify search.</p>
+        )
+    } else if (props.countries.length > 1) {
+        return (
+            <div>
+                {props.countries.map(country =>
+                <ManyCountries key={country.alpha3Code} country={country} />
+            )}
+            </div>
         )
     }
     console.log(props)
